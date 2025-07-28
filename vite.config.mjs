84d1761cdff5410e44170282c8ff34ff51417dac
Aarin Raj <93396600+rajaarin@users.noreply.github.com>
2025-07-28 17:03:38 +0530

    Create vite.config.mjs

---
+++ vite.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tagger from "@dhiwise/component-tagger";
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  build: {
+    outDir: "build",
+    chunkSizeWarningLimit: 2000,
+  },
+  resolve: {
+    alias: {
+      'components': path.resolve(__dirname, 'src/components'),
+      'pages': path.resolve(__dirname, 'src/pages'),
+      'utils': path.resolve(__dirname, 'src/utils'),
+      'contexts': path.resolve(__dirname, 'src/contexts'),
+      'styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
+  plugins: [tsconfigPaths(), react(), tagger()],
+  server: {
+    port: "4028",
+    host: "0.0.0.0",
+    strictPort: true,
+    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+  }
+});
